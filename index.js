66e9999de0bc67f1522ffa1c6ab598f4c961bc09
Priyanshu <prynsu@yahoo.com>
2021-07-21 16:05:51 +0530

    Express app setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hey I'm Cyanic API.")
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send("Hello! I'm Cyanic API.")
+})
+
+app.listen(3000, ()=> {
+    console.log("Server running on port 3000...")
+})
