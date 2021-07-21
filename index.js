c4891468bb17139f2276a7c80dacc0bfd3ce32a9
Priyanshu <prynsu@yahoo.com>
2021-07-21 16:46:56 +0530

    database connected

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.send("Hello! I'm Cyanic API.")
+app.use(bodyParser.json())
+
+// DB Connect
+const initializeDBConnect = require('./db/db.connect')
+initializeDBConnect()
+
+// Routers
+const videos = require('./routers/videos.v1.router')
+
+app.use('/videos', videos)
+
+app.get('/', (req, res) => {
+  res.send("Hello! I'm Cyanic API.")
 })
 
-app.listen(3000, ()=> {
-    console.log("Server running on port 3000...")
+app.listen(3000, () => {
+  console.log('Server running on port 3000...')
 })
