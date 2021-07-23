b14af5854a8457f0e5f9fe05e9cc6b777a9b1296
Priyanshu <prynsu@yahoo.com>
2021-07-24 01:50:21 +0530

    Made User Model

---
+++ models/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: "Cannot add a user without name"
+    },
+    email: {
+        type: String,
+        required: "Cannot add a user without a valid email address"
+    },
+    password: {
+        type: String,
+        required: "Cannot add a user without password"
+    },
+    playlists: {
+        type: Array,
+
+    }
+})
+const User = mongoose.model('User', UserSchema)
+module.exports = User
